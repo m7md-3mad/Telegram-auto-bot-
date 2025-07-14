b07ba0a401606566ae3a019fb240ddd869f115b7
m7md-3mad <150970719+m7md-3mad@users.noreply.github.com>
2025-07-14 05:20:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # توكن البوت
-BOT_TOKEN = "7674655190:AAHGQbac6F9ecwtp7fP0DK5B3_38cs0Jv1M"
+BOT_TOKEN = "7674655190:AAGuHfCmf3VX0qFXqUZi4eZOEgos_UnjjKY"
 
 # نصوص الأذكار
 MORNING_AZKAR = """
